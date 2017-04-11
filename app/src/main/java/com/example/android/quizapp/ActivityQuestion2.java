b1d2b8606971a0bfd24722c8b1c3162a73bce291
Mihaela <mihaela@L-108.local>
2017-04-11 17:25:16 +0200

    added the 2nd screen done

--- app/src/main/java/com/example/android/quizapp/ActivityQuestion2.java
+++ app/src/main/java/com/example/android/quizapp/ActivityQuestion2.java
@@ -1,13 +1,58 @@
 package com.example.android.quizapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class ActivityQuestion2 extends AppCompatActivity {
+
+    private CheckBox CheckBox1;
+    private CheckBox CheckBox2;
+    private CheckBox CheckBox3;
+    private CheckBox CheckBox4;
+    private int intValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question2);
+
+        Intent mIntent = getIntent();
+         intValue = mIntent.getIntExtra("countresult", 0);
+        CheckBox1 = (CheckBox) findViewById(R.id.checkBox1);
+        CheckBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        CheckBox3 = (CheckBox) findViewById(R.id.checkBox3);
+        CheckBox4 = (CheckBox) findViewById(R.id.checkBox4);
+
+        View.OnClickListener onClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), getString(R.string.checked_radio_button), Toast.LENGTH_SHORT).show();
+            }
+        };
+        CheckBox1.setOnClickListener(onClick);
+        CheckBox2.setOnClickListener(onClick);
+        CheckBox3.setOnClickListener(onClick);
+        CheckBox4.setOnClickListener(onClick);
+    }
+    public void nextscreen(View view) {
+        if (CheckBox1.isChecked() || CheckBox2.isChecked() || CheckBox3.isChecked() || CheckBox4.isChecked()) {
+            if (CheckBox1.isChecked()) {
+
+                intValue++;
+
+
+                Toast.makeText(getApplicationContext(), getString(R.string.right_answer), Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            Toast.makeText(getApplicationContext(), getString(R.string.no_option_choosen_question1), Toast.LENGTH_SHORT).show();
+        }
+        Intent myIntent = new Intent(ActivityQuestion2.this, ActivityQuestion3.class);
+        myIntent.putExtra("countresult", intValue);
+        startActivity(myIntent);
     }
 }
--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,17 +1,56 @@
 package com.example.android.quizapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
+    private RadioButton radioButton3;
+    private int countresult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         radioButton1 = (RadioButton) findViewById(R.id.option_1);
+         radioButton2 = (RadioButton) findViewById(R.id.option_2);
+         radioButton3 = (RadioButton) findViewById(R.id.option_3);
+
+        View.OnClickListener onClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), getString(R.string.checked_radio_button), Toast.LENGTH_SHORT).show();
+            }
+        };
+        radioButton1.setOnClickListener(onClick);
+        radioButton2.setOnClickListener(onClick);
+        radioButton3.setOnClickListener(onClick);
     }
+    public void changescreen(View view) {
 
+        if (radioButton1.isChecked() || radioButton2.isChecked() || radioButton3.isChecked()) {
 
+            if (radioButton1.isChecked()) {
+                countresult = countresult + 1;
+                Toast.makeText(getApplicationContext(), getString(R.string.right_answer), Toast.LENGTH_SHORT).show();
+                //   Intent intent = new Intent(getApplicationContext(), ActivityQuestion2.class);
+                // startActivity(intent);
+            }
 
+        } else {
+            Toast.makeText(getApplicationContext(), getString(R.string.no_option_choosen_question1), Toast.LENGTH_SHORT).show();
+        }
+        Intent myIntent = new Intent(MainActivity.this, ActivityQuestion2.class);
+        myIntent.putExtra("countresult", countresult);
+        startActivity(myIntent);
+
+    }
 
 }
